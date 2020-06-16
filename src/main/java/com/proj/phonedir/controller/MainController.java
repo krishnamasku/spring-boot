796787a00f53eb5786cece7ca5500744ef281477
Krishna Masuku <kmasuku@CSC-US-RSR-M001.COM>
2020-06-16 18:33:47 -0400

    adding controller changes

--- src/main/java/com/proj/phonedir/controller/MainController.java
+++ src/main/java/com/proj/phonedir/controller/MainController.java
@@ -2,9 +2,10 @@ package com.proj.phonedir.controller;
 
 import java.util.TreeSet;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,16 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.proj.phonedir.service.PhoneDirectoryService;
 
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class MainController {
-	
+
 	@Autowired
 	private PhoneDirectoryService phoneDirectoryService;
-	
-	@GetMapping(value={"/phone/{number}"}, produces="application/json")
-	public TreeSet<String> getPhoneNumbers(@PathVariable String number){
-		return phoneDirectoryService.getPhoneNumberCombinations(number);
+
+	@GetMapping(value = { "/phone/{number}" }, produces = "application/json")
+	public ResponseEntity<?> getPhoneNumbers(@PathVariable String number) {
+		TreeSet<String> resultSet = new TreeSet<String>();
+		ResponseEntity<?> resEntity = null;
+
+		if ((number != null || !"".equals(number)) && (number.length() == 7 || number.length() == 10))
+			resultSet = phoneDirectoryService.getPhoneNumberCombinations(number);
+
+		if (!resultSet.isEmpty()) {
+			resEntity = new ResponseEntity<TreeSet<String>>(resultSet, new HttpHeaders(), HttpStatus.OK);
+		} else {
+			resEntity = new ResponseEntity<String>("Invalid phone number", new HttpHeaders(), HttpStatus.BAD_REQUEST);
+		}
+		return resEntity;
 	}
-	
+
 }
